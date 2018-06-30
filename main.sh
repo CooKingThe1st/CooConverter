7b01dc86e68a4b0150984aef4fbf423f68ffe8e1
CroXX <39448470+Pega-Stellar@users.noreply.github.com>
2018-06-30 10:29:36 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -3,20 +3,23 @@ old_ifs=$IFS
 IFS=$(echo -en "\n\b")
 shopt -s nullglob
 chmod +x convert.sh
+if [ -e CooConverter ] ; then
+        :
+else
+        mkdir CooConverter
+fi
 for f in *; do
 if [ -d "$f" ]; then
-	cd "$f"
-	echo "convert  $f to pdf"
-	../convert.sh
-	cd ..
+        if [ "$f" = "CooConverter" ]; then
+                continue
+        fi
+        cd "$f"
+        echo "convert  $f to pdf"
+        ../convert.sh
+        cd ..
 fi
-done	
+done
 IFS=$old_ifs
-if [ -e CooConverter ] ; then
-	:
-else
-	mkdir CooConverter
-fi
-mv *.pdf CooConverter
+mv *.pdf "CooConverter"
 printf "Done "
 read -rn1
